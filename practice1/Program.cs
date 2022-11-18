ff6ebb1cf3c78b6a03ceeae40a523b7c835396b6
mehrnooshtabatabaei <118211083+mehrnooshtabatabaei@users.noreply.github.com>
2022-11-18 23:58:20 +0330

    Add files via upload

---
+++ practice1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+public class Demo
+{
+    public class Power
+    {
+        public long power_calc(int n)
+        {
+
+            double pow2 = n * n;
+            double pow3 = n * n * n;
+            Console.WriteLine("power of 2: " + pow2);
+            Console.WriteLine("power of 3: " + pow3);
+            return 0;
+        }
+    }
+
+
+
+    public static void Main(string[] args)
+    {
+        Power myPower = new Power();
+        Console.Write("Enter a number: ");
+        int input = Convert.ToInt32(Console.ReadLine());
+        myPower.power_calc(input);
+        Console.Read();
+    }
+
+}
---
+++ practice3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class program
+{
+    public class Age
+    {
+        int year, week, days, DAYSINWEEK = 7;
+
+        public void calc_age(int ndays) {
+
+            year = ndays / 365;
+            week = (ndays % 365) / DAYSINWEEK;
+            days = (ndays % 365) % DAYSINWEEK;
+            Console.WriteLine("{0} is equivalent to: \n{1} years, \n{2} weeks \n{3} days", ndays, year, week, days);
+
+        }
+    }
+
+    public static void Main()
+    {
+        Age myAge = new Age();
+        Console.Write("Enter the number of days: ");
+        int days = int.Parse(Console.ReadLine());
+        myAge.calc_age(days);
+
+
+        Console.ReadLine();
+    }
+}
